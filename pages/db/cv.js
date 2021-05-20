a6f5cd6f101030abf9ee097f7529af6c122f56bb
Zizwar <zizwar@gmail.com>
2021-05-20 21:14:09 +0000

    intianl db cv

---
+++ pages/db/cv.js
@@ -0,0 +1,21 @@
+export const cv = {
+    name: "",
+    subText: "",
+    tel: "",
+    mail: "",
+    avatar: "",
+    age: "",
+    skills: [
+        {
+            name: "Programming",
+            skill:
+                [
+                    { name: "HTML5", persent: 85 },
+                    { name: "CSS3 / SCSS", persent: 90 },
+                    { name: "Bootstrap", persent: 75 },
+                    { name: "Typescript", persent: 60 },
+                    { name: "JQuery", persent: 45 }
+                ]
+        }
+    ]
+}
