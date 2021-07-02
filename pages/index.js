6b179f4b193406048bd172e0c969873c50036647
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2021-07-02 07:14:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,106 +30,11 @@ export default function Home() {
                         <div className="skills">
                             {cv.skills.map(({ name = '', skill = [] }, index) => <Skills key={index} data={{ name, skill }} />)}
 
-                            <p>---- / SCSS ［90%］</p>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box"></span>
-                            <p>Javascript / jQuery ［40%］</p>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box"></span>
-                            <span className="box"></span>
-                            <span className="box"></span>
-                            <span className="box"></span>
-                            <span className="box"></span>
-                            <br /><br />
-                            <Line title={"Design"} />
-                            <p>Subversion ［90%］</p>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box"></span>
-                            <p>Adobe XD ［80%］</p>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box"></span>
-                            <span className="box"></span>
-                            <p>Sublime ［80%］</p>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box"></span>
-                            <span className="box"></span>
-                            <p>Photoshop ［90%］</p>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box"></span>
-                            <p>Illustrator ［90%］</p>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box"></span>
-                            <p>InDesign ［70%］</p>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box-ck"></span>
-                            <span className="box"></span>
-                            <span className="box"></span>
-                            <span className="box"></span>
+                           
                             <Line title={"Other"} />
-                            <p>Javascript</p>
-                            <p>Vue.js</p>
                             <p>Git</p>
-                            <p>Github</p>
-                            <p>WordPress</p>
-
-                            <Line title={"test"} />
-                            <p>小提琴</p>
-                            <p>吉他</p>
-                            <p>游泳</p>
+                            <p>Docker</p>
+                        
                         </div>
                     </div>
                     <div className="content">
@@ -149,13 +54,7 @@ export default function Home() {
                         <ul className="timeline">
 
 
-                            <div className="event" data-date="2012-2009">
-                                <h3>金甌女中 - 資料處理科</h3>
-                                <li><a target="_blank" href="http://webftp.cogsh.tp.edu.tw/sallywei/homework/web/danshui/html/1-1.html">2012全國慈善/科技/人文網頁設計比賽 人文組 第三名 <i className="fa fa-link"></i></a>
-                                    <li>技藝優良榮獲「傑出市長獎」畢業</li>
-                                    <li>考取20張專業證照 全校最多</li>
-                                </li>
-                            </div>
+                          
                         </ul>
                     </div>
                 </div>
