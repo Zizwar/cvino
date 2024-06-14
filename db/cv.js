b0f09ef8d6651a90cc7862829a8f60a58ec10cd1
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2024-06-14 06:24:24 +0100

    Create cv.js

---
+++ db/cv.js
@@ -0,0 +1,159 @@
+export const cv = {
+    name: "Ibrahim BIDI",
+    mail: "zizwar@gmail.com",
+    subName: "JavaScript ❣️",
+    
+    aboutMe: [
+        "About Ibrahim",
+        `Hello! I'm Ibrahim Bidi, a passionate and experienced developer specializing in JavaScript. From Frontend to Backend, mobile to desktop, robotics to blockchain, JavaScript is my realm. I have been dedicated to this language for 22 years, watching it grow from humble beginnings on Earth 🌍 to its expansive presence in space 🚀. JS is my heart and soul 🧡.
+        
+        Over the years, I've worked on numerous projects, contributed to the open-source community, and developed applications that have positively impacted many users. My journey began with creating the first Arabic keyboard for Windows 98 and has evolved into developing complex systems for various platforms.`
+    ],
+    mobile: "Mobile: +212648188181",
+    mailbox: "mailbox: zizwar@gmail.com",
+    avatar: "/avatar/ibrahim_bidi.jpg",
+    birth: "Birth: 1979",
+    info: "Basic information",
+    nameT: "Name: Ibrahim BIDI",
+    
+    site: [
+        {
+            text: "LinkedIn",
+            url: "https://www.linkedin.com/in/ibrahimbidi/"
+        },
+        {
+            text: "GitHub",
+            url: "https://github.com/ibrahimbidi"
+        }
+    ],
+    
+    skills: [
+        {
+            name: "Backend Technologies",
+            skill: [
+                { name: "NodeJs", percent: 90 },
+                { name: "DenoLand", percent: 95 },
+                { name: "ExpressJs", percent: 85 }
+            ]
+        },
+        {
+            name: "Frameworks & Libs",
+            skill: [
+                { name: "Reactjs/Nextjs/aleph", percent: 90 },
+                { name: "React Native", percent: 95 },
+                { name: "VueJs", percent: 85 }
+            ]
+        },
+        {
+            name: "Databases",
+            skill: [
+                { name: "MongoDB", percent: 95 },
+                { name: "MySQL/PostgreSQL/SQLite", percent: 85 },
+                { name: "Firebase/pushDB/Redis", percent: 90 }
+            ]
+        },
+        {
+            name: "Robotics & IoT",
+            skill: [
+                { name: "Johnny-Five", percent: 80 },
+                { name: "Cylonjs", percent: 70 },
+                { name: "NodeBots", percent: 70 }
+            ]
+        },
+        {
+            name: "Blockchain & Dapps",
+            skill: [
+                { name: "Web3", percent: 70 },
+                { name: "Ethersjs", percent: 70 },
+                { name: "Solidity", percent: 60 }
+            ]
+        }
+    ],
+    
+    works: [
+        {
+            name: "Key Projects",
+            date: "2000 - Present",
+            items: [
+                {
+                    name: "Key4Arab",
+                    events: [
+                        {
+                            text: "Developed an Arabic keyboard for non-Arabic Windows 98 systems.",
+                            link: ""
+                        }
+                    ]
+                },
+                {
+                    name: "WinoScript",
+                    events: [
+                        {
+                            text: "Created an Arabic programming framework based on JavaScript.",
+                            link: "https://github.com/Zizwar/winoscript"
+                        }
+                    ]
+                },
+                {
+                    name: "Tifino",
+                    events: [
+                        {
+                            text: "Developed Tifino, an application to support Tifinagh scripts in Windows, which received over 100,000 downloads.",
+                            link: "https://code.google.com/archive/p/tifino/"
+                        }
+                    ]
+                },
+                {
+                    name: "Ayat",
+                    events: [
+                        {
+                            text: "Developed the Quran application AYAT 2 for King Saud University.",
+                            link: "https://play.google.com/store/apps/details?id=com.ksu.ayat2&hl=en_US"
+                        }
+                    ]
+                },
+                {
+                    name: "Terminal*6",
+                    events: [
+                        {
+                            text: "Created a system for managing and processing data via SMS or Whatsapp using Node.js and MongoDB."
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+
+    experiences: {
+        title: "Competence experience",
+        items: [
+            {
+                name: "Web Development",
+                icon: "fa fa-tv",
+                experience: [
+                    "Worked with dozens of frontend and backend engineers and designers to develop project experience",
+                    "Experience in accessible web design for government units (achieving National Communications Commission barrier-free web page 2.0 A-level certification)",
+                    "SEO search engine optimization, Google keywords",
+                    "Experience developing web pages using the Bootstrap4/UIkit framework",
+                    "Experience designing RWD web pages using HTML5, SCSS, and jQuery",
+                    "Led a team of designers/interns to build websites and provided teaching experience",
+                    "Experience building websites using IIS",
+                    "Experience with FTP, remote deployer",
+                    "BI Software Interface Beautification Experience (KNIME, MicroStrategy, Tableau, IBM Cognos)"
+                ]
+            },
+            {
+                name: "Backend Development & Microservices",
+                icon: "fa fa-server",
+                experience: [
+                    "Extensive experience with Node.js and Deno for building scalable server-side applications",
+                    "Implemented microservices architecture for several projects, enhancing modularity and performance",
+                    "Proficient in using Express.js for creating RESTful APIs",
+                    "Experience with database design and management using MongoDB, MySQL, PostgreSQL, and Redis",
+                    "Developed and deployed serverless applications using AWS Lambda and Firebase Functions",
+                    "Worked on real-time data processing applications using WebSockets and pushDB",
+                    "Created secure and efficient authentication systems using JWT and OAuth"
+                ]
+            }
+        ]
+    }
+}
